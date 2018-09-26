1437d1104fcb041281c50ae87f3543eb1538a360
zhangxiaode <2279323443@qq.com>
2018-09-26 14:09:43 +0800

    移动互联

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,14 @@ export default {
       return document.body.clientHeight
     }
   },
+  beforeCreate () {
+    // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    //   window.location.href="./mobile"
+    // }
+    // else {
+    //   window.location.href="/"
+    // }
+  },
   created () {
     // 测试ajax
     // this.$ajax.get('/borrow/ordersInfo/getOrderList', {})
